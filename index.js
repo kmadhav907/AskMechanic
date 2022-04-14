e2e2dbcad9702a42b875dab69a690e122abc1d67
kmadhav907 <kmadhav907@gmail.com>
2022-04-14 21:42:01 +0530

    Added bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/home.html');
+});
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
 });
 app.listen(port, () => {
   console.log('Everything is running fine :)');
