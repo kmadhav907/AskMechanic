3008272a646b5b2f9e8ba670fc7c29dab21b98f2
Sunidhi <Sunidhihegde1999@gmail.com>
2022-04-22 18:22:53 +0530

    added the pages and navbar

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/about.html');
 });
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/TermsAndCondition.html');
+});
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/PrivacyPolicy.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
 app.listen(port, () => {
   console.log('Everything is running fine :)');
 });
